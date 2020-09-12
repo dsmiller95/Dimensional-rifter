4f55f3e797361b3ca7d55f5f1d306924835ae2ce
Dan Miller <dsmiller95@gmail.com>
2020-09-12 17:55:48 -0500

    only animate the resting spot when there is something resting in it

---
+++ Assets/Scripts/AnimatorParameterBinding.cs
@@ -0,0 +1,36 @@
+﻿using Assets.Scripts.Core;
+using System;
+using UniRx;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [RequireComponent(typeof(Animator))]
+    public class AnimatorParameterBinding : MonoBehaviour
+    {
+        [Serializable]
+        public struct BoolReferenceAnimatorBinding
+        {
+            public BooleanReference boolReference;
+            public string booleanParameterName;
+        }
+
+        public BoolReferenceAnimatorBinding[] bindings;
+
+        private Animator animator;
+
+        public void Awake()
+        {
+            animator = GetComponent<Animator>();
+            foreach (var binding in bindings)
+            {
+                binding.boolReference.ValueChanges
+                    .TakeUntilDisable(this)
+                    .Subscribe(newValue =>
+                    {
+                        animator.SetBool(binding.booleanParameterName, newValue);
+                    }).AddTo(this);
+            }
+        }
+    }
+}
