41ae07661344141a65ac149c8b95969c87ea6215
Dan Miller <dsmiller95@gmail.com>
2020-11-06 18:47:08 -0600

    add system which despawns loose items when they are completely picked up

---
+++ Assets/WorldObjects/Members/Items/DOTS/EmptyItemDespawnSystem.cs
@@ -0,0 +1,26 @@
+﻿using Unity.Entities;
+
+namespace Assets.WorldObjects.Members.Items.DOTS
+{
+    [UpdateInGroup(typeof(InitializationSystemGroup))]
+    public class EmptyItemDespaynSystem : SystemBase
+    {
+        EntityCommandBufferSystem despawnCommandBuffer => World.GetOrCreateSystem<EndInitializationEntityCommandBufferSystem>();
+        protected override void OnUpdate()
+        {
+            var commandBuffer = despawnCommandBuffer.CreateCommandBuffer().AsParallelWriter();
+
+            Entities
+                .WithAll<LooseItemFlagComponent>()
+                .ForEach((int entityInQueryIndex, Entity self, ref ItemAmountComponent amount) =>
+            {
+                if (amount.resourceAmount <= 0)
+                {
+                    commandBuffer.DestroyEntity(entityInQueryIndex, self);
+                }
+            }).ScheduleParallel();
+            despawnCommandBuffer.AddJobHandleForProducer(Dependency);
+        }
+
+    }
+}
