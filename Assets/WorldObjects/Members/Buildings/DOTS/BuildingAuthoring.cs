7292d3127b81b6f221786ce6c691fd43ae87c21f
Dan Miller <dsmiller95@gmail.com>
2020-11-08 13:22:05 -0600

    added new building authoring component, to cut out the extra stuff in the controller. switched to mesh renderer instead of sprite renderer for the wall.
    Have to figure out how to handle changing the color/display of the wall when built vs when it's a ghost. would be nice to just set two materials, one for ghost and one for built, and use those.

---
+++ Assets/WorldObjects/Members/Buildings/DOTS/BuildingAuthoring.cs
@@ -0,0 +1,61 @@
+﻿using Assets.WorldObjects.DOTSMembers;
+using Assets.WorldObjects.Inventories;
+using Assets.WorldObjects.Members.Items.DOTS;
+using Assets.WorldObjects.Members.Storage.DOTS;
+using Assets.WorldObjects.Members.Wall.DOTS;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Assets.WorldObjects.Members.Buildings.DOTS
+{
+    [DisallowMultipleComponent]
+    public class BuildingAuthoring : MonoBehaviour,
+        IConvertGameObjectToEntity
+    {
+        public Resource ItemTypeRequriement;
+        public float defaultResourceRequiredAmount;
+        public SuppliableType supplyClassification;
+
+        public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+        {
+            var parentCoordinatePosition = dstManager.GetComponentData<UniversalCoordinatePositionComponent>(entity);
+
+            var buildingEntity = dstManager.CreateEntity(
+                typeof(UniversalCoordinatePositionComponent),
+                typeof(IsNotBuiltFlag),
+                typeof(ItemAmountsDataComponent),
+                typeof(ItemAmountClaimBufferData),
+                typeof(SupplyTypeComponent),
+                typeof(BuildingChildComponent));
+
+            dstManager.SetComponentData(buildingEntity, parentCoordinatePosition);
+
+            dstManager.SetComponentData(buildingEntity, new ItemAmountsDataComponent
+            {
+                MaxCapacity = defaultResourceRequiredAmount,
+                TotalAdditionClaims = 0f,
+                LockItemDataBufferTypes = true
+            });
+            DynamicBuffer<ItemAmountClaimBufferData> itemAmounts = dstManager.GetBuffer<ItemAmountClaimBufferData>(buildingEntity);
+            itemAmounts.Add(new ItemAmountClaimBufferData
+            {
+                Type = ItemTypeRequriement,
+                Amount = 0f,
+                TotalSubtractionClaims = 0f
+            });
+            dstManager.SetComponentData(buildingEntity, new SupplyTypeComponent
+            {
+                SupplyTypeFlag = ((uint)1) << supplyClassification.ID
+            });
+
+            dstManager.SetComponentData(buildingEntity, new BuildingChildComponent
+            {
+                controllerComponent = entity
+            });
+            dstManager.AddComponentData(entity, new BuildingParentComponent
+            {
+                buildingEntity = buildingEntity
+            });
+        }
+    }
+}
