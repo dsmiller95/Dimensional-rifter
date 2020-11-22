c5c18b923043b3f2edced8d54709a7f85db2af0c
Dan Miller <dsmiller95@gmail.com>
2020-11-22 10:11:39 -0600

    created new OnReset behavior node

---
+++ Assets/Libraries/BehaviorTree/Factories/Decorators/OnResetFactory.cs
@@ -0,0 +1,16 @@
+﻿using BehaviorTree.Factories.FactoryGraph;
+using BehaviorTree.Nodes;
+using UnityEngine;
+
+namespace BehaviorTree.Factories
+{
+    [FactoryGraphNode("Decorator/OnReset", "OnReset", 1)]
+    public class OnResetFactory : DecoratorFactory
+    {
+        public NodeStatus ConstantResultOnEvaluate = NodeStatus.SUCCESS;
+        protected override BehaviorNode OnCreateNode(GameObject target)
+        {
+            return new EvaluateOnReset(ConstantResultOnEvaluate, child.CreateNode(target));
+        }
+    }
+}
---
+++ Assets/Libraries/BehaviorTree/Nodes/Decorator/EvaluateOnReset.cs
@@ -0,0 +1,25 @@
+﻿namespace BehaviorTree.Nodes
+{
+    /// <summary>
+    /// Do nothing, except when reset. On reset will evaluate the child node, throw out the result, and immediately reset the child node
+    /// </summary>
+    public class EvaluateOnReset : Decorator
+    {
+        private NodeStatus statusOnEvaluate;
+        public EvaluateOnReset(NodeStatus statusOnEvaluate, BehaviorNode child) : base(child)
+        {
+            this.statusOnEvaluate = statusOnEvaluate;
+        }
+
+        protected override NodeStatus OnEvaluate(Blackboard blackboard)
+        {
+            return statusOnEvaluate;
+        }
+
+        public override void Reset(Blackboard blackboard)
+        {
+            Child.Evaluate(blackboard);
+            base.Reset(blackboard);
+        }
+    }
+}
