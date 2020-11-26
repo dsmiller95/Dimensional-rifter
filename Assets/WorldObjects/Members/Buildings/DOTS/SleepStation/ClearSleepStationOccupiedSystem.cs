fa2f9c5a5e3ea3925449a09c214f878e8d528358
Dan Miller <dsmiller95@gmail.com>
2020-11-25 18:50:29 -0600

    clear sleep station occupied on load

---
+++ Assets/WorldObjects/Members/Buildings/DOTS/SleepStation/ClearSleepStationOccupiedSystem.cs
@@ -0,0 +1,17 @@
+﻿using Assets.WorldObjects.SaveObjects.SaveManager;
+using Unity.Entities;
+
+namespace Assets.WorldObjects.Members.Buildings.DOTS.SleepStation
+{
+    [UpdateInGroup(typeof(PostDeserialzeSystemGroup))]
+    public class ClearSleepStationOccupiedSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            Entities.ForEach((ref SleepStationOccupiedComponent occupied) =>
+            {
+                occupied.Occupied = false;
+            }).ScheduleParallel();
+        }
+    }
+}
